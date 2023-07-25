fc6c7211209ca1460d74dd9a0b610b8c0dc06437
luisc5martins <luisc.martinsjunior5@gmail.com>
2023-07-25 00:30:46 -0300

    first commit

---
+++ src/_data/livros.js
@@ -0,0 +1,86 @@
+export const livros = [
+  {
+    id: 1,
+    title: 'O Poderoso Chefão',
+    author: 'Mario Puzo',
+    price: 49.9,
+    img: 'https://m.media-amazon.com/images/I/31jGFI5wX2L._SX331_BO1,204,203,200_.jpg'
+  },
+  {
+    id: 2,
+    title: 'O Senhor dos Anéis - Box completo',
+    author: 'J. R. R. Tolkien',
+    price: 129.9,
+    img: 'https://m.media-amazon.com/images/I/715afDdgKfL.jpg'
+  },
+  {
+    id: 3,
+    title: 'O Pequeno Príncipe',
+    author: 'Antoine de Saint-Exupéry',
+    price: 19.9,
+    img: 'https://m.media-amazon.com/images/I/41afCn3PQUL._SY344_BO1,204,203,200_QL70_ML2_.jpg'
+  },
+  {
+    id: 4,
+    title: 'Dom Quixote',
+    author: 'Miguel de Cervantes',
+    price: 39.9,
+    img: 'https://m.media-amazon.com/images/I/513uHO8IDlL._SX359_BO1,204,203,200_.jpg'
+  },
+  {
+    id: 5,
+    title: 'O Conde de Monte Cristo',
+    author: 'Alexandre Dumas',
+    price: 69.9,
+    img: 'https://m.media-amazon.com/images/I/81ZswN9PVPL._AC_UL320_.jpg'
+  },
+  {
+    id: 6,
+    title: 'O Hobbit',
+    author: 'J. R. R. Tolkien',
+    price: 39.9,
+    img: 'https://m.media-amazon.com/images/I/91M9xPIf10L._AC_UL320_.jpg'
+  },
+  {
+    id: 7,
+    title: 'O Corpo Fala',
+    author: 'Pierre Weil e Roland Tompakow',
+    price: 47.7,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81b71E0VkVL.jpg'
+  },
+  {
+    id: 8,
+    title: 'Mortal Engines',
+    author: 'Philip Reeve',
+    price: 14.9,
+    img: 'https://m.media-amazon.com/images/I/51QRHv0QzjL._AC_UF1000,1000_QL80_.jpg'
+  },
+  {
+    id: 9,
+    title: 'Comunicação Não-violenta',
+    author: 'Marshall B. Rosenberg',
+    price: 71.0,
+    img: 'https://m.media-amazon.com/images/I/71vfutuuhnL.jpg'
+  },
+  {
+    id: 10,
+    title: 'A hipótese do Amor',
+    author: 'Ali Hazelwood',
+    price: 40.9,
+    img: 'https://m.media-amazon.com/images/I/81LTEfXYgcL.jpg'
+  },
+  {
+    id: 11,
+    title: 'Fahrenheit 451',
+    author: 'Ray Bradbury',
+    price: 29.9,
+    img: 'https://m.media-amazon.com/images/I/51tAD6LyZ-L.jpg'
+  },
+  {
+    id: 12,
+    title: 'Quem é Você, Alasca?',
+    author: 'John Green',
+    price: 39.9,
+    img: 'https://cdn.kobo.com/book-images/7a0bcb10-c51f-42c7-aec8-81d3ba10f211/1200/1200/False/quem-e-voce-alasca-edicao-comemorativa.jpg'
+  }
+]
