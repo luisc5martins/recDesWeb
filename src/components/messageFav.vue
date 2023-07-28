6b3af5351b30010bf0ee5620bf34eeb3de5fbcea
luisc5martins <luisc.martinsjunior5@gmail.com>
2023-07-28 14:35:10 -0300

    commit pc alisson

---
+++ src/components/messageFav.vue
@@ -0,0 +1,13 @@
+<template>
+
+    <p>Você não tem favoritos</p>
+    
+    </template>
+    
+    <style scoped>
+    p {
+        font-weight: bold;
+        font-size: 1.1rem;
+    }
+    
+    </style>
